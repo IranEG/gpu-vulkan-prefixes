c26a465bf79ae6dbb490518f8a9e046b826421d8
Iran Garcia <IranEG@users.noreply.github.com>
2023-06-19 18:37:45 -0400

    Adds script

---
+++ gpu_vulkan_prefixes.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# This script applies amdvlk-pro/amdvlk/radv/nvidia Vulkan driver for specific application.
+# You can see the list of available drivers with vulkaninfo utility, see the driverName and driverID lines.
+# 
+# Usage:
+#   <vk_radv|vk_amdvlk|vk_pro|vk_nvidia> <app and its parameters>
+# For example:
+#   $ vk_pro vkmark
+# This will start vkmark with AMDGPU PRO vulkan driver.
+
+# When amdvlk is installed, then VK_ICD_FILENAMES is ignored and AMD_VULKAN_ICD is read instead. However, AMD_VULKAN_ICD does not allow you (afaics) select pro driver.
+# So we revert this behavior to standard one.
+export DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1=1
+
+ICD_DIR="/usr/share/vulkan/icd.d"
+
+vk_radv() {
+    export VK_ICD_FILENAMES="${ICD_DIR}/radeon_icd.i686.json:${ICD_DIR}/radeon_icd.x86_64.json"
+}
+
+vk_amdvlk() {
+    export VK_ICD_FILENAMES="${ICD_DIR}/amd_icd32.json:${ICD_DIR}/amd_icd64.json"
+}
+
+vk_pro() {
+    export VK_ICD_FILENAMES="${ICD_DIR}/amd_pro_icd32.json:${ICD_DIR}/amd_pro_icd64.json"
+}
+
+vk_nvidia() {
+    export VK_ICD_FILENAMES="${ICD_DIR}/nvidia_icd.json"
+}
+
+if [[ $(basename $0) == vk_radv ]]; then
+    vk_radv
+elif [[ $(basename $0) == vk_amdvlk ]]; then
+    vk_amdvlk
+elif [[ $(basename $0) == vk_pro ]]; then
+    vk_pro
+elif [[ $(basename $0) == vk_nvidia]]; then
+    vk_nvidia
+else
+    echo "Unknown function"
+    exit 1
+fi
+
+# applied variables, now execute the rest of the command
+"$@"
